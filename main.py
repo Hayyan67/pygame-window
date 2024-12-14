798b326aa2413b402203bde44407751442843c36
Hayyan67 <humairabaig81@gmail.com>
2024-12-14 10:13:00 +0500

    initial commit

--- main.py
+++ main.py
@@ -16,6 +16,12 @@ def msg():
     m1="Hey "+ a1+ "\n Welcome to my window"
     L3 = Label(root, text=m1)
     L3.pack()
+radio=IntVar()
+L4 = Label(root,text="Select your gender: ")
+r1 = RADIOBUTTON(root, text="Male",Variable=radio, value=1)
+r1.pack()
+r2 = RADIOBUTTON(root, text="Female",Variable=radio, value=2)
+r2.pack()
 b1 =Button(root, text='Submit', bg='red', fg='white')
 b1.pack()
 root.mainloop()
